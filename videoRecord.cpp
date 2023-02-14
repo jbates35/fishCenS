7903b756dbb02a6439872a131374ad89c297adfe
Jimmy Bates <cbates21@my.bcit.ca>
2023-02-14 11:18:19 -0800

    Fixed constructor

--- videoRecord.cpp
+++ videoRecord.cpp
@@ -6,42 +6,6 @@ VideoRecord::VideoRecord()
 
 VideoRecord::~VideoRecord()
 { 
-	//Save video file to file
-	_video.release();
-
-	cout << "Video closed\n";
-	_log("Closing video");
-	_log("Video length: " + to_string(getTickCount()/getTickFrequency() - _startTime) + "\n");
-	
-	//Get mean of frame times
-	if (_frameCount > 1)
-	{
-		double frameMeanTime = (getTickCount() / getTickFrequency() - _startTime) / _frameCount;
-	
-		_log("Mean frame time: " + to_string(frameMeanTime * 1000) + "ms");
-		_log("Mean frame rate: " + to_string(1 / frameMeanTime) + "fps");	
-		
-		//Now get standard deviation
-		double frameSDSquared = 0;
-	
-		for (auto frameTime : _frameTimes)
-		{
-			frameSDSquared += (frameTime - frameMeanTime)*(frameTime - frameMeanTime);
-		}
-		double frameSD = sqrt(frameSDSquared / (_frameCount - 1.0));
-	
-		_log("Variance (standard deviation) of frame time: " + to_string(frameSD * 1000) + "ms");
-	}
-	
-	//Make text file and dump data
-	ofstream dataFile(_filePath + _fileName + ".txt");
-	
-	for (auto str : _loggerVec)
-	{
-		dataFile << str;
-	}
-
-	dataFile.close();	
 }
 
 void VideoRecord::run(Mat& frame, mutex& lock)
